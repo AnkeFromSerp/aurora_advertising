0cc21887708cb314988b786ae30436d2658bb22a
hunsergg203 <hunsergg@icloud.com>
2024-11-10 01:38:05 +0300

    Initial commit

---
+++ src/main/java/com/example/aurora/adversting/model/User.java
@@ -0,0 +1,65 @@
+package com.example.aurora.adversting.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 2681531852204068105L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String userId;
+    private String emailId;
+    private String password;
+    private boolean enabled;
+
+    @OneToOne(mappedBy = "users")
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+}
---
+++ src/test/java/com/example/aurora/adversting/AuroraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aurora.adversting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
